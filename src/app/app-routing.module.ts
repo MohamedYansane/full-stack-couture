cd72b8a23caaf49290d8ec8f684a7b9189d17859
Mohamed Yansane <yansane472@gmail.com>
2023-06-05 02:59:07 +0000

    configuring th router path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientListComponent } from './client-list/client-list.component';
+import { CouturierComponent } from './couturier/couturier.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"clients",component:ClientListComponent},
+  {path:"", redirectTo:"clients",pathMatch:"full"},
+  {path:"couturiers", component:CouturierComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
